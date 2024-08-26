cd865d8b403663fcbe08ce326fdc4f322a63eac3
mizdra <pp.mizdra@gmail.com>
2024-08-27 01:12:58 +0900

    add timezone

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ import { ScheduledComponent } from "@/components/ScheduledComponent";
 export default function Home() {
   return (
     <main>
-      <ScheduledComponent showAt={new Date("2024-01-01")}>
+      <ScheduledComponent showAt={new Date("2024-01-01T00:00:00.000Z")}>
         <h1>Happy New Year!</h1>
       </ScheduledComponent>
-      <ScheduledComponent showAt={new Date("2024-12-25")}>
+      <ScheduledComponent showAt={new Date("2024-12-25T00:00:00.000Z")}>
         <h1>Merry Christmas!</h1>
       </ScheduledComponent>
     </main>
