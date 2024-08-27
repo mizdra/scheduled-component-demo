7e67fed0974b5582cb3c8a49e455e66a1a635b55
mizdra <pp.mizdra@gmail.com>
2024-08-27 18:28:18 +0900

    campaign

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,10 @@ export default function Home() {
   return (
     <main>
       <ScheduledComponent showAt={new Date(`${thisYear}-01-01T00:00:00.000Z`)}>
-        <h1>Happy New Year!</h1>
+        <h1>Happy New Year Campaign!</h1>
       </ScheduledComponent>
       <ScheduledComponent showAt={new Date(`${thisYear}-12-25T00:00:00.000Z`)}>
-        <h1>Merry Christmas!</h1>
+        <h1>Christmas Campaign!</h1>
       </ScheduledComponent>
     </main>
   );
--- pages/client-component-leak-test.tsx
+++ pages/client-component-leak-test.tsx
@@ -6,10 +6,10 @@ export default function Home() {
   return (
     <main>
       <ScheduledClientComponent showAt={new Date(`${thisYear}-01-01T00:00:00.000Z`)}>
-        <h1>Happy New Year!</h1>
+        <h1>Happy New Year Campaign!</h1>
       </ScheduledClientComponent>
       <ScheduledClientComponent showAt={new Date(`${thisYear}-12-25T00:00:00.000Z`)}>
-        <h1>Merry Christmas!</h1>
+        <h1>Christmas Campaign!</h1>
       </ScheduledClientComponent>
     </main>
   );
