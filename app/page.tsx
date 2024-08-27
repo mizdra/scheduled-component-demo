1da21399a0cce1e37229958db56eb73df11cd6e8
mizdra <pp.mizdra@gmail.com>
2024-08-27 18:18:42 +0900

    use this year

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { ScheduledComponent } from "@/components/ScheduledComponent";
 
+const thisYear = new Date().getFullYear();
+
 export default function Home() {
   return (
     <main>
-      <ScheduledComponent showAt={new Date("2024-01-01T00:00:00.000Z")}>
+      <ScheduledComponent showAt={new Date(`${thisYear}-01-01T00:00:00.000Z`)}>
         <h1>Happy New Year!</h1>
       </ScheduledComponent>
-      <ScheduledComponent showAt={new Date("2024-12-25T00:00:00.000Z")}>
+      <ScheduledComponent showAt={new Date(`${thisYear}-12-25T00:00:00.000Z`)}>
         <h1>Merry Christmas!</h1>
       </ScheduledComponent>
     </main>
